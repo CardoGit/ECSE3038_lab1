6661b22fe31ff7bb71cfcaf7569bdccf084651ff
Ricardo Harrison <ricardocsn01@gmail.com>
2025-01-31 08:05:32 -0500

    Added Potential Divider Function

--- app6.py
+++ app6.py
@@ -6,3 +6,14 @@ def parallel(resistors):
     print(f"{result} ohms")
 
 parallel([330, 1000, 2200])
+
+
+#Question 2
+
+def potential_divider(voltage, resistors):
+    total_resistance = sum(resistors)
+    for r in resistors:
+        voltage_drop = round(voltage * (r / total_resistance), 2)
+        print(f"{voltage_drop}v")
+
+potential_divider(9, [3000, 1000])
